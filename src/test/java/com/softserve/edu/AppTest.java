0d57b22134aac12a0beb305f4c2cb2aacdbaa381
bebebe <hahaha@gmail.com>
2023-10-17 00:37:38 +0300

    Tepmlate

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,19 @@
+package com.softserve.edu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp1() {
+        System.out.println("class AppTest: testApp1()");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void testApp2() {
+        System.out.println("class AppTest: testApp2()");
+        Assertions.assertTrue(true);
+    }
+}
