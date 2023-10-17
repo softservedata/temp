38a4dd051473ff5d42447d510ab18099b3dc84fe
Jaroslav <garasym@gmail.com>
2023-10-17 13:10:04 +0300

    WebDriver

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,13 +1,31 @@
 package com.softserve.edu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        //System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // 0 by default
+        driver.manage().window().maximize();
+        //
+        driver.get("http://taqc-opencart.epizy.com/");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.name("search")).sendKeys("mac");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+        //
+        Thread.sleep(8000); // For Presentation
+        driver.quit();
     }
 }
--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -1,7 +1,13 @@
 package com.softserve.edu;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class AppTest {
 
@@ -12,8 +18,24 @@ public class AppTest {
     }
 
     @Test
-    public void testApp2() {
-        System.out.println("class AppTest: testApp2()");
-        Assertions.assertTrue(true);
+    public void checkMac() throws InterruptedException {
+        //System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // 0 by default
+        driver.manage().window().maximize();
+        //
+        driver.get("http://taqc-opencart.epizy.com/");
+        driver.get("https://demo.opencart.com/");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.name("search")).sendKeys("mac");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+        //
+        Thread.sleep(8000); // For Presentation
+        driver.quit();
     }
 }
