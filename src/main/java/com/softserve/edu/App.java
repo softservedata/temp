ea485045f47230c2271b8d4157606eb44b9110f7
Jaroslav <garasym@gmail.com>
2023-10-17 14:47:47 +0300

    HtmlUnitDriver

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,16 +1,31 @@
 package com.softserve.edu;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 
 public class App {
-    public static void main(String[] args) throws InterruptedException {
+
+    private static void takeScreenShot(WebDriver driver) throws IOException {
+        String currentTime = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-S").format(new Date());
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("./" + currentTime + "_screenshot.png"));
+    }
+
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+        //
+        /*
         //System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -27,5 +42,55 @@ public class App {
         //
         Thread.sleep(8000); // For Presentation
         driver.quit();
+        */
+        /*
+        WebDriverManager.phantomjs().setup();
+        WebDriver driver = new PhantomJSDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // 0 by default
+        driver.manage().window().maximize();
+        //
+        driver.get("http://www.google.com");
+        System.out.println("\tdriver.get http://www.google.com DONE");
+        System.out.println("\tCurrent title is: " + driver.getTitle());
+        Thread.sleep(1000);
+        //
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!");
+        //element.submit();
+        System.out.println("\telement.sendKeys Cheese! DONE");
+        System.out.println("\tPage title is: " + driver.getTitle());
+        element.submit();
+        Thread.sleep(1000);
+        System.out.println("\tPage title is: " + driver.getTitle());
+        //
+        System.out.println("\tTaking First Screenshot ...");
+        Thread.sleep(2000);
+        takeScreenShot(driver);
+        //
+        driver.findElement(By.partialLinkText("Cheese - Wikipedia")).click();
+        System.out.println("\tELEMENT IS " + driver.findElement(By.id("firstHeading")).getText());
+        //Assert.assertEquals(driver.findElement(By.id("firstHeading")).getText(), "Cheese");
+        //
+        System.out.println("\tTaking Second Screenshot ...");
+        // File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // FileUtils.copyFile(scrFile, new File("./screenshot1.png"));
+        Thread.sleep(2000);
+        takeScreenShot(driver);
+        System.out.println("\tDone Screenshot");
+        //
+        driver.quit();
+        */
+        //
+        WebDriver driver = new HtmlUnitDriver(false);
+        //((HtmlUnitDriver) driver).setJavascriptEnabled(true); // TODO Enable CSS
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+        driver.get("https://demo.opencart.com/");
+        System.out.println("\tdriver.get http://www.google.com DONE");
+        System.out.println("\tCurrent title is: " + driver.getTitle());
+        Thread.sleep(2000);
+        //
+        driver.quit();
+        System.out.println("\tdone");
     }
 }
